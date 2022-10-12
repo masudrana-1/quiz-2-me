2c5124e1c6294c9c037e1df5c625b0292b000f74
masudrana-1 <masudrana58855@gmail.com>
2022-10-12 19:50:53 +0600

    netlify issue fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,6 @@ const Footer = () => {
     return (
         <div className='mt-5 p-10 bg-black text-white'>
             <h1>@All Rights Reserved By Quiz2Me</h1>
-            <div>
-            </div>
         </div>
     );
 };
