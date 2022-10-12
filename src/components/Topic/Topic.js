dc70099d43c2e68dc46f8898fe57cb8cbfe0f0af
masudrana-1 <masudrana58855@gmail.com>
2022-10-12 20:03:43 +0600

    netlify reload issue fixed

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import { ArrowSmallRightIcon } from '@heroicons/react/24/solid'
 
 const Topic = ({ quiz }) => {
     const { id, name, logo, total } = quiz;
-
-    const [allquiz, setAllquiz] = useState([]);
-
-    useEffect(() => {
-        fetch(`https://openapi.programming-hero.com/api/quiz/${id}`)
-            .then(res => res.json())
-            .then(data => setAllquiz(data))
-    }, [])
-
-    // console.log(allquiz);
-
-    const quizloader = (id) => {
-        allquiz(id);
-    };
-
-    // console.log(allquiz)
-
 
     return (
         <div className='bg-gradient-to-r from-violet-800 via-blue-600 to-green-600 m-auto p-10 w-10/12 rounded-2xl shadow-2xl'>
@@ -30,7 +13,6 @@ const Topic = ({ quiz }) => {
             <div className='flex lg:justify-around flex-col lg:flex-row justify-center items-center mt-5 text-center'>
                 <h4 className='flex text-lg font-medium'>Topic: <p className='text-orange-600 ml-4'>{name}</p></h4>
                 <p className='text-lg font-medium'>Total Quiz: <small className='text-orange-600 text-lg ml-4'>{total}</small> </p>
-                {/* <button className='bg-orange-500' onClick={() => quizloader(id)}><Link to=></button>Show all quiz</Link></button> */}
                 <Link to={`/quiz/${id}`} className='flex ml-2 bg-amber-500 px-3 py-1 rounded-lg'>show all<ArrowSmallRightIcon className="h-5 w-8 mt-1" /></Link>
             </div>
         </div>
