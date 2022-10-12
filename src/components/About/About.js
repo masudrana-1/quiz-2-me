9b2f4d94b4cca3b033fd7fc3133ea9f5b0b4c27c
masudrana-1 <masudrana58855@gmail.com>
2022-10-12 18:58:41 +0600

    change font size in about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,16 +5,16 @@ const About = () => {
     return (
         <div className='h-screen'>
             <div className='mt-6'>
-                <h1 className='text-xl text-black mb-2'>To know more about</h1>
-                <p className='text-xl text-black mb-2'>Please contruct us</p>
-                <h1 className='text-xl text-black mb-2'>Mobile : +9907837478</h1>
-                <h1 className='text-xl text-black mb-2'>Telephone: +9864716543</h1>
-                <h1 className='text-xl text-black mb-2'>Hotline: 16543 (24 hours)</h1>
+                <h1 className='text-xl font-bold text-black mb-2'>To know more about</h1>
+                <p className='text-xl font-bold text-black mb-2'>Please contruct us</p>
+                <h1 className='text-xl font-bold text-black mb-2'>Mobile : +9907837478</h1>
+                <h1 className='text-xl font-bold text-black mb-2'>Telephone: +9864716543</h1>
+                <h1 className='text-xl font-bold text-black mb-2'>Hotline: 16543 (24 hours)</h1>
                 <div className='font-sans'>
-                    <Link className='ml-3 text-purple-900' to='web.facebook.com'>Facebook</Link>
-                    <Link className='ml-3 text-purple-900' to='www.twitter.com'>Twitter</Link>
-                    <Link className='ml-3 text-purple-900' to='www.linkedin.com'>Linkedin</Link>
-                    <Link className='ml-3 text-purple-900' to='www.github.com'>Github</Link>
+                    <Link className='ml-3 font-semibold text-purple-900' to='web.facebook.com'>Facebook</Link>
+                    <Link className='ml-3 font-semibold text-purple-900' to='www.twitter.com'>Twitter</Link>
+                    <Link className='ml-3 font-semibold text-purple-900' to='www.linkedin.com'>Linkedin</Link>
+                    <Link className='ml-3 font-semibold text-purple-900' to='www.github.com'>Github</Link>
                 </div>
                 <p></p>
             </div>
